ef3d5b4f5bd9cfe7010308cbdbb3d603f92c45b9
MiliGaleano <mmiligaleano@gmail.com>
2021-03-16 22:19:11 -0300

    Updates

---
+++ precache-manifest.3df6a961b06d5292e70e3ec4009a6421.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba0ebe72877f93085b809e2cbb68a83",
+    "url": "/BLessed-React-App/index.html"
+  },
+  {
+    "revision": "bc66603549aa786943f6",
+    "url": "/BLessed-React-App/static/css/2.e0c80bc5.chunk.css"
+  },
+  {
+    "revision": "ab8b60598ac4c55cec69",
+    "url": "/BLessed-React-App/static/css/main.9d09f6be.chunk.css"
+  },
+  {
+    "revision": "bc66603549aa786943f6",
+    "url": "/BLessed-React-App/static/js/2.e816e51c.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/BLessed-React-App/static/js/2.e816e51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8b60598ac4c55cec69",
+    "url": "/BLessed-React-App/static/js/main.fd3cb8a3.chunk.js"
+  },
+  {
+    "revision": "4c2b60eaa457343b8e5d",
+    "url": "/BLessed-React-App/static/js/runtime-main.e12f0a1a.js"
+  },
+  {
+    "revision": "7d23f27fa239754b0ed6aa18f0695415",
+    "url": "/BLessed-React-App/static/media/loading.7d23f27f.gif"
+  },
+  {
+    "revision": "a212ddfec2fcaa1d50b4af3f70169f78",
+    "url": "/BLessed-React-App/static/media/lupa.a212ddfe.png"
+  },
+  {
+    "revision": "0ebd0add29fdfb454ad3ce31bc4ca449",
+    "url": "/BLessed-React-App/static/media/menu.0ebd0add.png"
+  },
+  {
+    "revision": "653e8252afd1f389b86e4a0f4a79185c",
+    "url": "/BLessed-React-App/static/media/pinkmilk.653e8252.png"
+  }
+]);
